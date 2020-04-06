6d22643bca2fdf49da7d70377d77110c8b310396
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-04-07 00:19:16 +0200

    uploaded assignments

---
+++ Scripten AJAX 1/form_1.php
@@ -0,0 +1,12 @@
+<?php
+	$firstName = $_GET['firstName'];
+	$lastName = $_GET['lastName'];
+	$age = $_GET['age'];
+	$email = $_GET['email'];
+	echo "<h2>Response Demo Form</h2><h3> ";
+	echo "You submitted the following information<br><ul>";
+	echo "<li>First name: <strong> $firstName</strong></li>";
+	echo "<li>Last name: <strong> $lastName</strong></li>";
+	echo "<li>Age:  $age</li>";
+	echo "<li>E-mail:  $email</li>";
+?>
---
+++ Scripten AJAX 1/script.js
@@ -0,0 +1,23 @@
+//bind de HTML id's
+let firstName = document.getElementById("firstName");
+let age = document.getElementById("age");
+let submitButton = document.getElementById("submitButton");
+let responseHere = document.getElementById("responseHere");
+//click event listener
+submitButton.addEventListener("click", ajax);
+//ajax functie
+function ajax(){
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            responseHere.innerHTML = this.responseText;
+        }
+    };
+    let httpString = "form_1.php?firstName=" + firstName.value + "form_1.php?lastName=" + lastName.value + "form_1.php?age=" + age.value + "form_1.php?email=" + email.value;
+    //debuggen laat de http string zien
+    console.log(httpString);
+    //bereid de ajax actie voor
+    xmlhttp.open("GET", httpString, true);
+    xmlhttp.send(); // doe het
+    
+}
